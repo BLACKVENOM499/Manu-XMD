79425fcba22327cf5271e2a17c67bddbe0806f45
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-02-24 12:37:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'raYy2ZJJ#M8jDUUHyk65sg1ldtTAEekcrf7WodXt5EVhHrQgcre4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'gGVzmKAJ#WfHx95lCa6oGleCReWQAe2Y1wbjESeOaB8QVvK4C4bE' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
